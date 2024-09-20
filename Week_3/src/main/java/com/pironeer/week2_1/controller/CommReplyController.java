7b6bb2e2c949c461133e637087c6b15e7631fa06
Jaeyeong Won <devlog.jae13@gmail.com>
2024-09-20 23:12:39 +0900

    :sparkles: : swagger 설명 추가

--- Week_3/src/main/java/com/pironeer/week2_1/controller/CommReplyController.java
+++ Week_3/src/main/java/com/pironeer/week2_1/controller/CommReplyController.java
@@ -4,6 +4,7 @@ import com.pironeer.week2_1.dto.request.CommReplyCreateRequest;
 import com.pironeer.week2_1.dto.request.CommentUpdateRequest;
 import com.pironeer.week2_1.dto.response.CommReplyResponse;
 import com.pironeer.week2_1.service.CommReplyService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,30 +18,28 @@ public class CommReplyController {
   private final CommReplyService commReplyService;
 
   @PostMapping
+  @Operation(summary = "대댓글 달기")
   public ResponseEntity<?> create(@RequestBody CommReplyCreateRequest request) {
     commReplyService.save(request);
     return ResponseEntity.ok().build();
   }
 
   @GetMapping("/{commReplyId}")
+  @Operation(summary = "대댓글 조회")
   public ResponseEntity<CommReplyResponse> read(@PathVariable("commReplyId") Long id) {
     CommReplyResponse response = commReplyService.findById(id);
     return ResponseEntity.ok().body(response);
   }
 
-  @PatchMapping
-  public ResponseEntity<CommReplyResponse> update(@RequestBody CommentUpdateRequest request) {
-    CommReplyResponse response = commReplyService.update(request);
-    return ResponseEntity.ok().body(response);
-  }
-
   @DeleteMapping("/{commReplyId}")
+  @Operation(summary = "대댓글 삭제")
   public ResponseEntity<?> delete(@PathVariable("commReplyId") Long id) {
     commReplyService.deleteById(id);
     return ResponseEntity.ok().build();
   }
 
   @GetMapping
+  @Operation(summary = "모든 대댓글 조회")
   public ResponseEntity<List<CommReplyResponse>> readAll() {
     List<CommReplyResponse> responses = commReplyService.findAll();
     return ResponseEntity.ok().body(responses);
